611218b912f27e20558f9d6a48bcb5319e526e1f
Carolina Alonso <c.alonsolp@gmail.com>
2022-01-10 14:50:05 +0100

    fix: delete duplicate uppercase directory

--- Js/Constant.js
+++
@@ -1,8 +0,0 @@
-// <----------------------------------------->
-// DECLARAMOS LAS CONSTANTES DE LOS CONTROLES
-// <----------------------------------------->
-
-const KEY_RIGHT = 68;
-const KEY_LEFT = 65;
-const KEY_DOWN = 83;
-const KEY_UP = 87;
